9968700fec65a52c23851c6b99d40cda2bb3b06f
H0rn0chse <h0rn0chse@gmx.de>
2021-07-11 15:54:42 +0200

    Updated version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,6 +16,10 @@
                 </h1>
 
                 <p class="subheading font-weight-regular">
+                    Got some Updates! (v0.0.3)
+                </p>
+
+                <p class="subheading font-weight-regular">
                     Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
                 </p>
             </v-col>
