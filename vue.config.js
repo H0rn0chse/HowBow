995095f5cdf28df39ed87784d69b0f882f4289ad
H0rn0chse <h0rn0chse@gmx.de>
2021-07-11 14:30:13 +0200

    Updated Icon

--- vue.config.js
+++ vue.config.js
@@ -12,6 +12,26 @@ module.exports = {
         electronBuilder: {
             mainProcessFile: "background.js",
             builderOptions: {
+                extraFiles: [
+                    {
+                        from: "./LICENSE",
+                        to: "./LICENSE.HowBow.txt"
+                    }
+                ],
+                win: {
+                    extraFiles: [
+                        {
+                            from: "build/win/",
+                            to: ".",
+                            filter: [
+                                "HowBow.VisualElementsManifest.xml"
+                            ]
+                        }
+                    ],
+                    extraResources: [
+                        "build/win/*"
+                    ]
+                },
             }
         }
     }
